fa95b367a7c47e2dd09562cf8ee8a82ee1db25de
Ayah Ahmad <ayah.ash62@gmail.com>
2021-05-25 17:48:37 +0300

    this works!

--- functions.js
+++ functions.js
@@ -7,7 +7,8 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+  
+ console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,8 +21,15 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+  
+  if (n%2 === 0)
+  return false
+
+  else
+  return true
 }
+
+  isOdd(6);
 
 /**
  * oddsSmallerThan(n):
@@ -33,8 +41,13 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+
+   
+  return Math.floor(n/2);
+
 }
+
+
 
 /**
  * squareOrDouble(n):
@@ -47,7 +60,17 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+  
+
+  if (n%2 === 0)
+
+  return n+n;
+ 
+
+  else
+ 
+return n*n;
 }
+
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
